8bc52e3459e733d419359e63af4324a3ded903ed
Gouripriya Manga <mangagouripriya@gmail.com>
2026-08-15 10:08:59 +0530

    Complete Version 2 CRUD management

--- main.py
+++ main.py
@@ -24,7 +24,13 @@ menu_choice = st.sidebar.radio(
         "Add Student",
         "Add Teacher",
         "Display Students",
+        "Search Student",
+        "Update Student",
+        "Delete Student",
         "Display Teachers",
+        "Search Teacher",
+        "Update Teacher",
+        "Delete Teacher",
         "List College"
     )
 )
@@ -98,25 +104,283 @@ elif menu_choice == "Add Teacher":
                 st.success("teacher added successfully")
 
 elif menu_choice == "Display Students":
-    clgname = st.selectbox("Choose college", [c.cname for c in st.session_state.colleges])
-    clg = find_college(clgname)
-    st.subheader(f"List of students in {clgname}")
-    if clg.students:
-        for i,s in enumerate(clg.students,1):
-            st.write(f"{i}.{s.name}")
+    if not st.session_state.colleges:
+        st.info("Please insert a college first")
     else:
-        st.warning("No student Found")
+        clgname = st.selectbox(
+            "Choose college",
+            [c.cname for c in st.session_state.colleges]
+        )
+
+        clg = find_college(clgname)
+
+        st.subheader(f"List of students in {clgname}")
+
+        if clg.students:
+            for i, s in enumerate(clg.students, 1):
+                st.write(f"{i}. {s.name}")
+        else:
+            st.warning("No student Found")
+
+elif menu_choice == "Update Student":
+
+    if not st.session_state.colleges:
+        st.info("Please insert a college first")
+
+    else:
+        clgname = st.selectbox(
+            "Choose college",
+            [c.cname for c in st.session_state.colleges]
+        )
+
+        clg = find_college(clgname)
+
+        roll = st.text_input("Enter Student Roll Number")
+
+        if st.button("FIND STUDENT"):
+
+            found_student = None
+
+            for s in clg.students:
+                if s.rollno == roll:
+                    found_student = s
+                    break
+
+            if found_student:
+
+                st.success("Student found!")
+
+                new_name = st.text_input(
+                    "Enter New Name",
+                    value=found_student.name
+                )
+
+                new_branch = st.text_input(
+                    "Enter New Branch",
+                    value=found_student.branch
+                )
+
+                if st.button("UPDATE"):
+
+                    found_student.name = new_name
+                    found_student.branch = new_branch
+
+                    st.success("Student updated successfully!")
+
+            else:
+                st.error("Student not found")
+
+elif menu_choice == "Delete Student":
+
+    if not st.session_state.colleges:
+        st.info("Please insert a college first")
+
+    else:
+        clgname = st.selectbox(
+            "Choose college",
+            [c.cname for c in st.session_state.colleges]
+        )
+
+        clg = find_college(clgname)
+
+        roll = st.text_input("Enter Student Roll Number")
+
+        if st.button("DELETE"):
+
+            found_student = None
+
+            for s in clg.students:
+                if s.rollno == roll:
+                    found_student = s
+                    break
+
+            if found_student:
+
+                clg.students.remove(found_student)
+
+                st.success("Student deleted successfully!")
+
+            else:
+                st.error("Student not found")
+
 
 elif menu_choice == "Display Teachers":
-    clgname = st.selectbox("Choose college", [c.cname for c in st.session_state.colleges])
-    clg = find_college(clgname)
-    st.subheader(f"List of teachers in {clgname}")
-    if clg.teachers:
-        for i,t in enumerate(clg.teachers,1):
-            st.write(f"{i}.{t.name}")
+    if not st.session_state.colleges:
+        st.info("Please insert a college first")
     else:
-        st.warning("No Teacher Found")
+        clgname = st.selectbox(
+            "Choose college",
+            [c.cname for c in st.session_state.colleges]
+        )
 
+        clg = find_college(clgname)
+
+        st.subheader(f"List of teachers in {clgname}")
+
+        if clg.teachers:
+            for i, t in enumerate(clg.teachers, 1):
+                st.write(f"{i}. {t.name}")
+        else:
+            st.warning("No Teacher Found")
+
+
+elif menu_choice == "Search Teacher":
+
+
+    if not st.session_state.colleges:
+        st.info("Please insert a college first")
+
+
+    else:
+        clgname = st.selectbox(
+            "Choose college",
+            [c.cname for c in st.session_state.colleges]
+        )
+
+
+        clg = find_college(clgname)
+
+
+        tname = st.text_input("Enter Teacher Name")
+
+
+        if st.button("SEARCH"):
+
+
+            found_teacher = None
+
+
+            for t in clg.teachers:
+                if t.name == tname:
+                    found_teacher = t
+                    break
+
+
+            if found_teacher:
+                st.success("Teacher found!")
+                st.write("Name:", found_teacher.name)
+                st.write("Branch:", found_teacher.branch)
+                st.write("Subject:", found_teacher.subject)
+
+
+            else:
+                st.error("Teacher not found")
+
+
+elif menu_choice == "Update Teacher":
+
+
+    if not st.session_state.colleges:
+        st.info("Please insert a college first")
+
+
+    else:
+        clgname = st.selectbox(
+            "Choose college",
+            [c.cname for c in st.session_state.colleges]
+        )
+
+
+        clg = find_college(clgname)
+
+
+        tname = st.text_input("Enter Teacher Name")
+
+
+        if st.button("FIND TEACHER"):
+
+
+            found_teacher = None
+
+
+            for t in clg.teachers:
+                if t.name == tname:
+                    found_teacher = t
+                    break
+
+
+            if found_teacher:
+
+
+                st.success("Teacher found!")
+
+
+                new_name = st.text_input(
+                    "Enter New Name",
+                    value=found_teacher.name
+                )
+
+
+                new_branch = st.text_input(
+                    "Enter New Branch",
+                    value=found_teacher.branch
+                )
+
+
+                new_subject = st.text_input(
+                    "Enter New Subject",
+                    value=found_teacher.subject
+                )
+
+
+                if st.button("UPDATE"):
+
+
+                    found_teacher.name = new_name
+                    found_teacher.branch = new_branch
+                    found_teacher.subject = new_subject
+
+
+                    st.success("Teacher updated successfully!")
+
+
+            else:
+                st.error("Teacher not found")
+
+
+elif menu_choice == "Delete Teacher":
+
+
+    if not st.session_state.colleges:
+        st.info("Please insert a college first")
+
+
+    else:
+        clgname = st.selectbox(
+            "Choose college",
+            [c.cname for c in st.session_state.colleges]
+        )
+
+
+        clg = find_college(clgname)
+
+
+        tname = st.text_input("Enter Teacher Name")
+
+
+        if st.button("DELETE"):
+
+
+            found_teacher = None
+
+
+            for t in clg.teachers:
+                if t.name == tname:
+                    found_teacher = t
+                    break
+
+
+            if found_teacher:
+
+
+                clg.teachers.remove(found_teacher)
+
+
+                st.success("Teacher deleted successfully!")
+
+
+            else:
+                st.error("Teacher not found")
 elif menu_choice == "List College":
     if not st.session_state.colleges:
         st.info("Please insert a college first")
